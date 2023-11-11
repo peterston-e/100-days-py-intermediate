dca3aeeb894b049b52b43e17ab279eab7466acc9
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-11 07:57:33 +0000

    added new methods py file

---
+++ Day-17-make-class/adding_methods.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+
+
+user_1 = User("001", "pete")
+print(user_1.id)
