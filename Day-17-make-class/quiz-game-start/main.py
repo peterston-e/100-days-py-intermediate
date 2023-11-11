c45d74495d5f09d81f0f744702a00ff4eb647494
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-11 09:53:47 +0000

    added question bank to main.py

---
+++ Day-17-make-class/quiz-game-start/main.py
@@ -0,0 +1,23 @@
+from question_model import Question
+from data import question_data
+
+# create question bank
+question_bank = []
+
+# loop through data
+# extract question and answer
+# create question object
+# add to question bank
+for question in question_data:
+    text = question["text"]
+    answer = question["answer"]
+    new_question = Question(text, answer)
+    question_bank.append(new_question)
+
+print(question_bank[0].text)
+print(question_bank[0].answer)
+# So, now it's a list of objects, it's easier to access it.
+# If it was a list of dictionaries, you would have to use strings
+# to access all the keys and easy to make mistakes.
+# Now you can use the dot notation and get the helper box pop-up
+# And you still have access to all the list modules also.
