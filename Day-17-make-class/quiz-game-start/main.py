883bb5dd8b535218df17758be0c232621ed7532d
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-11 11:12:19 +0000

    added quizzbrain class with atri. and next q. function

--- Day-17-make-class/quiz-game-start/main.py
+++ Day-17-make-class/quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizzBrain
 
 # create question bank
 question_bank = []
@@ -13,11 +14,17 @@ for question in question_data:
     answer = question["answer"]
     new_question = Question(text, answer)
     question_bank.append(new_question)
-
-print(question_bank[0].text)
-print(question_bank[0].answer)
+# print(question_bank[0].text)
+# print(question_bank[0].answer)
 # So, now it's a list of objects, it's easier to access it.
 # If it was a list of dictionaries, you would have to use strings
 # to access all the keys and easy to make mistakes.
 # Now you can use the dot notation and get the helper box pop-up
 # And you still have access to all the list modules also.
+
+
+# Todo ask user the question
+# todo check the answer
+# todo check if at the end of the quiz
+quiz = QuizzBrain(question_bank)
+quiz.next_question()
---
+++ Day-17-make-class/quiz-game-start/quiz_brain.py
@@ -0,0 +1,19 @@
+# todo quiz brain class
+# attributes: question number = 0, question list
+# methods: next_question
+
+# create class
+# add thee two attributes and the list is initialised when
+# quizbrain in created by passing in the question bank
+
+class QuizzBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        question = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q{self.question_number}. {question.text}. (True/False)?: ")
+        # return user_answer
