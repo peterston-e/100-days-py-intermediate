fc88b4027f48ea3af5d911f72caae63d7fa8e598
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-11 07:54:52 +0000

    added notes to attributes lesson

--- Day-17-make-class/main.py
+++ Day-17-make-class/main.py
@@ -1,9 +1,23 @@
 class User:
     def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
+        self.id = user_id  # the attribute becomes the parameter passed in
+        self.username = username  # self is the object
+        self.followers = 0  # default value is zero
 
 
 user_1 = User("001", "pete")
 print(user_1.id)
+
+# the __init__(self, attribute_1, attribute_2)
+# this is a constructor that constructs the object
+# this runs when the object is initialised from the class and
+# runs whatever is below it essentially setting up the attributes
+
+# you can also just add new attributes
+#  user_1.home_town = "brighton"
+
+# a function attached to a class is called a method.
+# user = User() initialise
+#     pass
+# self is the actual object that's being created
+#
