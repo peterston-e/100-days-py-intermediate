ab755100a8192a118cdf96d5de5c59837115f60a
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-11 08:24:57 +0000

    added notes and code to methods file

--- Day-17-make-class/adding_methods.py
+++ Day-17-make-class/adding_methods.py
@@ -3,7 +3,18 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1  # updates attribute of parameter
+        self.following += 1  # updates the object itself
 
 
 user_1 = User("001", "pete")
-print(user_1.id)
+user_2 = User("002", "dave")
+
+user_1.follow(user_2)  # calls the method attached to the object
+print(f"User 1 - {user_1.followers}")
+print(f"User 1 - {user_1.following}")
+print(f"User 2 - {user_2.followers}")
+print(f"User 2 - {user_2.following}")
