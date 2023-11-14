3e6c2d257cd80b7176de1d06470b47fc934f99ca
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-14 20:58:19 +0000

    moved snake functionality from main to snake

--- Day-20-snake-game/main.py
+++ Day-20-snake-game/main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -6,32 +7,35 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
-SIZE = 40
-snake_length = 3
+# SIZE = 40
+# snake_length = 3
 
-color = ["red", "white", "blue"]
-new_col = 0
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+# color = ["red", "white", "blue"]
+# new_col = 0
+# starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+# segments = []
 
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.penup()
-    new_segment.color(color[new_col])
-    new_segment.goto(position)
-    segments.append(new_segment)
-    new_col += 1 % 3
+# for position in starting_positions:
+#     new_segment = Turtle(shape="square")
+#     new_segment.penup()
+#     new_segment.color(color[new_col])
+#     new_segment.goto(position)
+#     segments.append(new_segment)
+#     new_col += 1 % 3
+
+snake = Snake()
 
 game_on = True
 while game_on:
     screen.update()
-    time.sleep(0.4)
+    time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):  # start, stop, step
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
+    # for seg_num in range(len(segments) - 1, 0, -1):  # start, stop, step
+    #     new_x = segments[seg_num - 1].xcor()
+    #     new_y = segments[seg_num - 1].ycor()
+    #     segments[seg_num].goto(new_x, new_y)
+    # segments[0].forward(20)
 
 screen.exitonclick()
--- Day-20-snake-game/snake.py
+++ Day-20-snake-game/snake.py
@@ -1,5 +1,21 @@
+from turtle import Turtle
+
 class Snake:
 
     def __init__(self):
         self.start_positions = [(0, 0), (-20, 0), (-40, 0)]
         self.segments = []
+        for position in self.start_positions:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):  # start, stop, step
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
