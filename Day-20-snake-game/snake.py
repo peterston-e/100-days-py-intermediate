ed2713c9c7bb00b0616b29b470cad808ae862fb9
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-14 21:11:54 +0000

    refined Snake class

--- Day-20-snake-game/snake.py
+++ Day-20-snake-game/snake.py
@@ -1,11 +1,14 @@
 from turtle import Turtle
-
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 class Snake:
 
     def __init__(self):
-        self.start_positions = [(0, 0), (-20, 0), (-40, 0)]
         self.segments = []
-        for position in self.start_positions:
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
             new_segment = Turtle(shape="square")
             new_segment.color("white")
             new_segment.penup()
@@ -18,4 +21,4 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(20)
+        self.segments[0].forward(MOVE_DISTANCE)
