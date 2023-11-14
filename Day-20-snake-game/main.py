915a8898c5c0e91023ead5593aae493d29387c91
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-14 07:14:07 +0000

    setup 3 snake pieces

--- Day-20-snake-game/main.py
+++ Day-20-snake-game/main.py
@@ -5,6 +5,19 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+SIZE = 40
+snake_length = 3
+y_position = 0.0
+
+
+for square in range(0, snake_length):
+    new_turtle = Turtle(shape="square")
+    new_turtle.penup()
+    new_turtle.color("white")
+    new_turtle.goto(square * -20.0, 0)
+
+
+
 
 
 
