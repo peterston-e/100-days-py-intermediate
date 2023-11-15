d8e7b739511da8b173154e921bdb07b0c6ec5112
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-15 17:35:56 +0000

    added food function

--- Day-20-snake-game/main.py
+++ Day-20-snake-game/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,9 +22,12 @@ screen.onkey(snake.right, "Right")
 game_on = True
 while game_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.2)
 
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        # detect collision with food
 
 
 screen.exitonclick()
