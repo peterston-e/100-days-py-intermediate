d112954eb3ec6f886973768ff9c0c388bb898742
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-15 19:03:12 +0000

    extend tail function

--- Day-20-snake-game/main.py
+++ Day-20-snake-game/main.py
@@ -32,9 +32,17 @@ while game_on:
         snake.extend()
         scoreboard.increase_score()
 
-#     detect wall
+    # detect wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_on = False
         scoreboard.game_over()
+
+    # detect tail collision
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
