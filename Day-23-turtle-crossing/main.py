adb7b9a1b47635e595e356f964d42168ed59d315
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-11-19 17:46:21 +0000

    added player move function

--- Day-23-turtle-crossing/main.py
+++ Day-23-turtle-crossing/main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -7,6 +7,10 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+player = Player()
+
+screen.listen()
+screen.onkey(player.up, "Up")
 
 game_is_on = True
 while game_is_on:
--- Day-23-turtle-crossing/player.py
+++ Day-23-turtle-crossing/player.py
@@ -1,7 +1,16 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.seth(90)
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.goto((self.xcor(), self.ycor() + MOVE_DISTANCE))
